dab232ce9e8b8ce2b2ba4f273ab670568974f98e
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-10-05 14:41:15 -0500

    Inte

--- Soldado/src/main.cpp
+++ Soldado/src/main.cpp
@@ -1,19 +1,43 @@
+#include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 
-const char* ssid= "Red_Mi";
+const char* ssid = "Red_Mi";
 const char* password = "12345678";
 
-
-void setup() {  
+void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Conectando a la red Wi-Fi...");
   }
-  Serial.println("Connected to the WiFi network");  
+  Serial.println("Conectado a la red Wi-Fi");
+  Serial.print("Dirección IP del esclavo: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  delay(1000);
+  WiFiServer server(9600); // Crea un servidor en el puerto 9600
+  server.begin();
+
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("Cliente conectado");
+
+    while (client.connected()) {
+      if (client.available()) {
+        String mensaje = client.readStringUntil('\n');
+        Serial.println("Mensaje recibido del maestro: " + mensaje);
+
+        // Aquí puedes realizar alguna acción en respuesta al mensaje recibido
+
+        // Envía una respuesta al maestro (opcional)
+        client.println("Respuesta desde el esclavo");
+      }
+    }
+
+    client.stop();
+    Serial.println("Cliente desconectado");
+  }
 }
