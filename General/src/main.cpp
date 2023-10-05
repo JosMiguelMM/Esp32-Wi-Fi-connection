4b9009ed93e43600eb35f8addd10119d343743af
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-10-05 15:18:00 -0500

    connection established, improve synchronization

--- General/src/main.cpp
+++ General/src/main.cpp
@@ -2,11 +2,12 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-const char* ssid = "Red_Mi";
-const char* password = "12345678";
-const char* esclavoIP = "192.168.4.2"; // Dirección IP del módulo esclavo en la red generada
+const char *ssid = "Red_Mi";
+const char *password = "12345678";
+const char *esclavoIP = "192.168.4.2"; // Dirección IP del módulo esclavo en la red generada
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   WiFi.softAP(ssid, password);
 
@@ -15,23 +16,28 @@ void setup() {
   Serial.println(WiFi.softAPIP());
 }
 
-void loop() {
+void loop()
+{
   WiFiClient client;
+  delay(3000);
 
   // Conectar con el módulo esclavo
-  if (client.connect(esclavoIP, 9600)) {
+  if (client.connect(esclavoIP, 9600))
+  {
     Serial.println("Conexión con el módulo esclavo establecida.");
-    
+
     // Enviar un mensaje al módulo esclavo
     client.println("Hola desde el maestro");
 
     // Esperar una respuesta del esclavo (opcional)
-    while (client.available() == 0);
-    String respuesta = client.readStringUntil('\n');
-    Serial.println("Respuesta del esclavo: " + respuesta);
+    // while (client.available() == 0);
+    // String respuesta = client.readStringUntil('\n');
+    // Serial.println("Respuesta del esclavo: " + respuesta);
 
     client.stop(); // Cierra la conexión
-  } else {
+  }
+  else
+  {
     Serial.println("No se pudo conectar con el módulo esclavo.");
   }
 
--- Soldado/src/main.cpp
+++ Soldado/src/main.cpp
@@ -2,42 +2,54 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 
-const char* ssid = "Red_Mi";
-const char* password = "12345678";
+const char *ssid = "Red_Mi";
+const char *password = "12345678";
+WiFiServer server(9600);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Conectando a la red Wi-Fi...");
   }
   Serial.println("Conectado a la red Wi-Fi");
   Serial.print("Dirección IP del esclavo: ");
   Serial.println(WiFi.localIP());
+
+  server.begin();
 }
 
-void loop() {
-  WiFiServer server(9600); // Crea un servidor en el puerto 9600
-  server.begin();
-
+void loop()
+{
   WiFiClient client = server.available();
-  if (client) {
+  Serial.println("Esperando cliente..." + client);
+  delay(2000);
+  if (client)
+  {
     Serial.println("Cliente conectado");
-
-    while (client.connected()) {
-      if (client.available()) {
+  regreso:
+    while (client.connected())
+    {
+      if (client.available())
+      {
         String mensaje = client.readStringUntil('\n');
         Serial.println("Mensaje recibido del maestro: " + mensaje);
 
         // Aquí puedes realizar alguna acción en respuesta al mensaje recibido
 
         // Envía una respuesta al maestro (opcional)
-        client.println("Respuesta desde el esclavo");
+        // client.println("Respuesta desde el esclavo");
       }
     }
-
+    goto regreso;
     client.stop();
     Serial.println("Cliente desconectado");
+  }
+  else
+  {
+    Serial.println("No se ha establecido coneccion");
   }
 }
