12c2f1ab8c297403cab67c5737e35a3dce1a5b87
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-10-04 23:46:56 -0500

    Conecta, esclavo genera errores

--- General/src/main.cpp
+++ General/src/main.cpp
@@ -1,40 +1,39 @@
+#include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 
-const char *ssid = "Red_Mi";
-const char *password = "12345678";
-const char *esclavoIP = "192.168.4.2";
+const char* ssid = "Red_Mi";
+const char* password = "12345678";
+const char* esclavoIP = "192.168.4.2"; // Dirección IP del módulo esclavo en la red generada
 
-void setup()
-{
-  // put your setup code here, to run once:
+void setup() {
   Serial.begin(9600);
   WiFi.softAP(ssid, password);
 
   Serial.println("WiFi AP is running");
-  Serial.print(WiFi.softAPIP());
+  Serial.print("Dirección IP del maestro: ");
+  Serial.println(WiFi.softAPIP());
 }
 
-void loop()
-{
+void loop() {
   WiFiClient client;
 
   // Conectar con el módulo esclavo
-  if (client.connect(esclavoIP, 9600))
-  {
+  if (client.connect(esclavoIP, 9600)) {
     Serial.println("Conexión con el módulo esclavo establecida.");
-
+    
     // Enviar un mensaje al módulo esclavo
     client.println("Hola desde el maestro");
 
-    // // Esperar una respuesta del esclavo (opcional)
-    // while (client.available() == 0);
-    // String respuesta = client.readStringUntil('\n');
-    // Serial.println("Respuesta del esclavo: " + respuesta);
-    //client.stop(); // Cierra la conexión
-  }
-  else
-  {
+    // Esperar una respuesta del esclavo (opcional)
+    while (client.available() == 0);
+    String respuesta = client.readStringUntil('\n');
+    Serial.println("Respuesta del esclavo: " + respuesta);
+
+    client.stop(); // Cierra la conexión
+  } else {
     Serial.println("No se pudo conectar con el módulo esclavo.");
   }
-  delay(3000);
+
+  delay(1000);
 }
